02ff95593272c6a22d5bcf7ad29c21ab7bafbdc7
Chia-Che Tsai <chitsai@cs.stonybrook.edu>
2018-08-11 13:51:13 -0700

    update the openjdk directory

--- LibOS/shim/test/apps/openjdk/run-java.sh
+++ LibOS/shim/test/apps/openjdk/run-java.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname ${BASH_SOURCE[0]})
 
-JAVA_FLAGS="-Xmx64m -XX:ReservedCodeCacheSize=8m -XX:+UseSerialGC -XX:+DisableExplicitGC -XX:-UseCompressedClassPointers -XX:-UseCompressedOops"
+JAVA_FLAGS="-Xmx1024m -XX:ReservedCodeCacheSize=64m -XX:+UseBoundThreads -XX:+UseSerialGC -XX:+DisableExplicitGC -XX:-UseCompressedClassPointers -XX:-UseCompressedOops"
 
 set -x
 exec ./java.manifest $JAVA_FLAGS "$@"
